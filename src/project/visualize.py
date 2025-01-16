cf0e744805e872f71aa8b0358918b0ef1aa15526
Rachael Marie De Vries <gsn245@styx.binf.ku.dk>
2025-01-16 22:48:10 +0100

    feat: visualize.py

---
+++ src/project/visualize.py
@@ -0,0 +1,26 @@
+from project.configs import DatasetConfig, OptimizerConfig, TrainConfig
+from project.data import get_processed_datasets
+import matplotlib.pyplot as plt
+import numpy as np
+from transformers import AutoTokenizer
+
+# load data
+train_dataset = get_processed_datasets(
+        split="auxiliary_train",
+        mode="multiclass",
+        subset_size=1000,
+        )
+
+# look at data label distribution
+labels = [item["labels"] for item in train_dataset]
+label_counts = np.unique(labels, return_counts=True)
+print(label_counts)
+plt.bar(label_counts[0], label_counts[1])
+plt.savefig("label_distribution.png")
+
+# print an example Q&A
+tokenizer = AutoTokenizer.from_pretrained("answerdotai/ModernBERT-base")
+train_example = tokenizer.decode(train_dataset[0]["input_ids"]).replace("[PAD]", "")
+answer = train_dataset[0]["labels"]
+print(f"Question: {train_example}, Answer: {answer}")
+
