2c6572641764c7686228e08ece0f89ed6fdac434
Andreas Geert Motzfeldt <andreas@motzfeldt.dk>
2025-01-20 13:09:18 +0100

    fix: dvc actions

--- src/project/datasets_statistics.py
+++ src/project/datasets_statistics.py
@@ -1,72 +1,70 @@
-import pathlib
+from collections import Counter
 
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 import typer
 
 from project.data import load_from_dvc
 
 
-def grouped_bar_chart(data: dict[str, np.ndarray], labels: np.ndarray):
+def grouped_bar_chart(data: dict[str, Counter]):
     """Create a grouped bar chart."""
     # Create bar chart with a better color scheme
-    x = np.arange(len(labels))  # the label locations
-    width = 0.35  # the width of the bars
+    x = np.arange(4)  # the label locations
+    width = 0.2  # the width of the bars
+    totals = {split: sum(cnt.values()) for split, cnt in data.items()}
 
-    colors = plt.cm.get_cmap("tab10", len(data))
+    colors = plt.colormaps.get_cmap("Set2")
     fig, ax = plt.subplots()
-    for i, split in enumerate(labels):
-        ax.bar(x + i * width, data[split], width, label=split, color=colors(i))
+    for i, split in enumerate(data):
+        cnt = data[split]
+        tot = totals[split]
+        ax.bar(x + i * width, [cnt[label] / tot for label in [0, 1, 2, 3]], width, label=split, color=colors(i))
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_xlabel("Labels")
-    ax.set_ylabel("Counts")
+    ax.set_ylabel("Frequency (%)")
+    ax.set_ylim(0, 0.5)
     ax.set_title("Labels count by split")
-    ax.set_xticks(x + width / len(labels))
-    ax.set_xticklabels(labels)
+    ax.set_xticks(x + width / 4)
+    ax.set_xticklabels(["A", "B", "C", "D"])
     ax.legend()
 
     fig.tight_layout()
     return fig
 
 
-def histogram_bar_chart(data: dict[str, np.ndarray]):
-    """Create a histogram bar chart."""
+def histogram_bar_chart(data: dict[str, Counter]):
+    """Create a density plot with a line for each split."""
     fig, ax = plt.subplots()
-    for split, values in data.items():
-        ax.hist(values, bins=30, alpha=0.5, label=split)
-    ax.set_xlabel("Query length")
-    ax.set_ylabel("Counts")
-    ax.set_title("Query length distribution by split")
+    for split, cnt in data.items():
+        y = list(cnt.values())
+        sns.kdeplot(y, ax=ax, label=split)
+    ax.set_xlabel("Question length")
+    ax.set_ylabel("Density")
+    ax.set_title("Question length distribution by split")
     ax.legend()
     fig.tight_layout()
     return fig
 
 
-def main(datadir: str = "data/processed/mmlu_tiny_raw", output_dir: str = "visuals") -> None:
+def main(datadir: str = "mmlu_tiny_raw") -> None:
     """Compute dataset statistics."""
     dataset = load_from_dvc(filepath=datadir)
     print(f"Dataset: {dataset}")
 
-    labels_count = {split: np.unique(dset["labels"], return_counts=True) for split, dset in dataset.items()}
-    query_length = {split: np.array([len(query) for query in dset["query"]]) for split, dset in dataset.items()}
+    labels = {split: dset["answer"] for split, dset in dataset.items()}
 
     # Prepare data for plotting
-    splits = list(labels_count.keys())
+    splits = list(dataset.keys())
 
     # Labels distribution
-    labels = np.unique(np.concatenate([labels_count[split][0] for split in splits]))
-    counts = {split: labels_count[split][1] for split in splits}
+    labels_count = {split: Counter(lst) for split, lst in labels.items()}
 
-    labels_chart = grouped_bar_chart(counts, labels)
-    query_lengths_chart = histogram_bar_chart(query_length)
-
-    # Create the output directory if it does not exist
-    output_path = pathlib.Path(output_dir)
-    output_path.mkdir(parents=True, exist_ok=True)
-
-    labels_chart.savefig(output_path / "labels_count.png")
-    query_lengths_chart.savefig(output_path / "query_length_distribution.png")
+    labels_chart = grouped_bar_chart(labels_count)
+    labels_chart.savefig("labels_count.png")
+    plt.close()
 
 
 if __name__ == "__main__":
