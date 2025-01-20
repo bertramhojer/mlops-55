7faedeabcf1e49d823644ac703b9c51987979eb7
Andreas Geert Motzfeldt <andreas@motzfeldt.dk>
2025-01-20 11:29:56 +0100

    fix: add matplotlib and dataset statistics

--- src/project/datasets_statistics.py
+++ src/project/datasets_statistics.py
@@ -1,14 +1,52 @@
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
 import typer
 
-from project.data import MMLUDataset
+from project.data import load_from_dvc
+from project.settings import settings
 
 
-def main(datadir: str = "data/processed/test_binary_n100.dataset") -> None:
+def main(datadir: str = "data/processed/mmlu_tiny.dataset", output_dir: str = "output") -> None:
     """Compute dataset statistics."""
-    dataset = MMLUDataset.from_file(filepath=datadir)
+    dataset = load_from_dvc(filepath=datadir)
     print(f"Dataset: {dataset}")
 
-    print("\n")
+    labels_count = {split: np.unique(dset["labels"], return_counts=True) for split, dset in dataset.items()}
+
+    # Prepare data for plotting
+    splits = list(labels_count.keys())
+    labels = np.unique(np.concatenate([labels_count[split][0] for split in splits]))
+    counts = {split: labels_count[split][1] for split in splits}
+
+    # Create bar chart
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = plt.subplots()
+    for i, split in enumerate(splits):
+        ax.bar(x + i * width, counts[split], width, label=split)
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_xlabel("Labels")
+    ax.set_ylabel("Counts")
+    ax.set_title("Labels count by split")
+    ax.set_xticks(x + width / len(splits))
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    fig.tight_layout()
+    plt.show()
+
+    # Create the output directory if it does not exist
+    output_path = pathlib.Path(settings.PROJECT_DIR / output_dir)
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    # Save the plot to a folder
+    output_path = f"{output_dir}/labels_count_by_split.png"
+    fig.savefig(output_path)
+    print(f"Plot saved to {output_path}")
 
 
 if __name__ == "__main__":
--- src/project/settings.py
+++ src/project/settings.py
@@ -18,21 +18,5 @@ class ProjectSettings(pydantic_settings.BaseSettings):
         "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
     )
 
-    @classmethod
-    def settings_customise_sources(
-        cls,
-        settings_cls: type[pydantic_settings.BaseSettings],
-        init_settings: pydantic_settings.InitSettingsSource,
-        env_settings: pydantic_settings.EnvSettingsSource,
-        dotenv_settings: pydantic_settings.DotEnvSettingsSource,
-        file_secret_settings: pydantic_settings.SecretsSettingsSource,
-    ) -> tuple[pydantic_settings.PydanticBaseSettingsSource, ...]:
-        """See: https://docs.pydantic.dev/latest/concepts/pydantic_settings/#customise-settings-sources."""
-        _init_kwargs = {k: v for k, v in init_settings.init_kwargs.items() if v is not None}
-        if _init_kwargs:
-            init_settings.init_kwargs = _init_kwargs
-            return init_settings, env_settings, dotenv_settings, file_secret_settings
-        return env_settings, dotenv_settings, file_secret_settings
-
 
 settings = ProjectSettings()  # type: ignore  # noqa: PGH003
