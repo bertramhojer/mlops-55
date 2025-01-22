5636ca3936132d321d2a1639b7cc597d3349f43e
gsn245 <gsn245@bio.ku.dk>
2025-01-22 20:48:11 +0100

    trigger dvc workflow

--- src/project/settings.py
+++ src/project/settings.py
@@ -4,6 +4,7 @@ import pydantic
 import pydantic_settings
 import torch
 import wandb
+from loguru import logger
 
 
 class ProjectSettings(pydantic_settings.BaseSettings):
@@ -12,9 +13,9 @@ class ProjectSettings(pydantic_settings.BaseSettings):
     model_config = pydantic_settings.SettingsConfigDict(extra="ignore", protected_namespaces=("settings_",))
 
     PROJECT_DIR: Path = Path(__file__).parent.parent.parent
-    WANDB_PROJECT: str = "ModernBERTQA"
-    WANDB_ENTITY: str = "mlops_55"
-    WANDB_API_KEY: str = pydantic.Field(..., description="Wandb API key")
+    WANDB_PROJECT: str | None = "ModernBERTQA"
+    WANDB_ENTITY: str | None = "mlops_55"
+    WANDB_API_KEY: str | None = pydantic.Field(default=None, description="Wandb API key")
     DEVICE: torch.device = torch.device(
         "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
     )
@@ -26,4 +27,7 @@ class ProjectSettings(pydantic_settings.BaseSettings):
 
 
 settings = ProjectSettings()  # type: ignore  # noqa: PGH003
-wandb.login()
+if all([settings.WANDB_PROJECT, settings.WANDB_ENTITY, settings.WANDB_API_KEY]):
+    wandb.login()
+else:
+    logger.warning("Wandb not configured. Skipping login.")
