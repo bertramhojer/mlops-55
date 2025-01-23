84540a7095db127b38c16f997ad75720b4d8c0be
gsn245 <gsn245@bio.ku.dk>
2025-01-23 15:38:23 +0100

    fix logguru error

--- src/project/settings.py
+++ src/project/settings.py
@@ -3,8 +3,7 @@ from pathlib import Path
 import pydantic
 import pydantic_settings
 import torch
-import wandb
-from loguru import logger
+from logguru import logger
 
 
 class ProjectSettings(pydantic_settings.BaseSettings):
@@ -13,9 +12,9 @@ class ProjectSettings(pydantic_settings.BaseSettings):
     model_config = pydantic_settings.SettingsConfigDict(extra="ignore", protected_namespaces=("settings_",))
 
     PROJECT_DIR: Path = Path(__file__).parent.parent.parent
-    WANDB_PROJECT: str | None = "ModernBERTQA"
-    WANDB_ENTITY: str | None = "mlops_55"
-    WANDB_API_KEY: str | None = pydantic.Field(default=None, description="Wandb API key")
+    WANDB_PROJECT: str = "ModernBERTQA"
+    WANDB_ENTITY: str = "mlops_55"
+    WANDB_API_KEY: str = pydantic.Field(..., description="Wandb API key")
     DEVICE: torch.device = torch.device(
         "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
     )
@@ -27,7 +26,3 @@ class ProjectSettings(pydantic_settings.BaseSettings):
 
 
 settings = ProjectSettings()  # type: ignore  # noqa: PGH003
-if all([settings.WANDB_PROJECT, settings.WANDB_ENTITY, settings.WANDB_API_KEY]):
-    wandb.login()
-else:
-    logger.warning("Wandb not configured. Skipping login.")
